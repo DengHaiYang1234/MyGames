d862c65c57ade174198a4d23817242db42962b03
Everglow <30494065+DengHaiYang1234@users.noreply.github.com>
2018-11-12 22:45:22 +0800

    2018.11.12
    基于MVC的俄罗斯方块

---
+++ 001.TetrisByMVC/TetrisProject/Assets/Scripts/Ctrl/Ctrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl : MonoBehaviour
+{
+    [HideInInspector]
+    public Model model;
+    [HideInInspector]
+    public View view;
+
+    private FSMSystem fsm;
+
+
+    private void Awake()
+    {
+        model = GameObject.FindGameObjectWithTag("Model").GetComponent<Model>();
+        view = GameObject.FindGameObjectWithTag("View").GetComponent<View>();
+        MakeFSM();
+    }
+
+
+    void MakeFSM()
+    {
+        fsm = new FSMSystem();
+        FSMState[] states = GetComponentsInChildren<FSMState>();
+        foreach (FSMState state in states)
+        {
+            fsm.AddState(state);
+        }
+
+        MenuState s = GetComponentInChildren<MenuState>();
+        fsm.SetCurrentState(s);
+    }
+
+
+
+}
